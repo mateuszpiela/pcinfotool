545eaebc272aa9bf718b883a990aed4a06a56bf5
Mateusz Pieła <20213336+mateuszpiela@users.noreply.github.com>
2024-04-09 23:07:56 +0200

    add OsReport to ReportMaker struct

--- reportmaker/reportmaker.go
+++ reportmaker/reportmaker.go
@@ -2,12 +2,15 @@ package reportmaker
 
 import (
 	"github.com/mateuszpiela/pcinfotool/hardwareinfo"
+	"github.com/mateuszpiela/pcinfotool/osinfo"
 )
 
 type ReportMaker struct {
+	ReportCreatedAt string
 	ReportVersion string
 	ReportSoftware string
 	HardwareReport *hardwareinfo.HwInfoReport
+	OsReport *osinfo.OsReport
 }
 
 func GenerateReport() {
